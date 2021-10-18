15bed8c2d99351b878f4a360e0e43d469748fa76
sjsteer <shaynesteer@gmail.com>
2021-10-18 14:10:00 +1100

    cleaned up sankey code and added reference

--- sankey/fuel_type_sankey.js
+++ sankey/fuel_type_sankey.js
@@ -1,6 +1,6 @@
-var units = "% of Fuel Supply in 2020-2021";
+// Based on https://bl.ocks.org/d3noob/06e72deea99e7b4859841f305f63ba85
 
-// set the dimensions and margins of the graph
+var units = "% of Fuel Supply in 2020-2021";
 var margin = {top: 10, right: 10, bottom: 10, left: 10},
     width = 500 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
@@ -9,11 +9,7 @@ var margin = {top: 10, right: 10, bottom: 10, left: 10},
 var formatNumber = d3.format(",.1f"),    // zero decimal places
     format = function(d) { return formatNumber(d) + units; },
 
-// Define two colour palletes, one for states and one for fuel types
-// color = d3.scaleOrdinal()
-//                   .domain(['Biomass', 'Black coal', 'Liquid Fuel', 'Gas', 'Battery', 'Wind', 'Hydro', 'Brown coal'])
-//                   .range(['#66a61e', '#666666', '#757ob3', '']);
-
+// Define colour palletes for the states and fuels
 color = d3.scaleOrdinal(d3.schemeCategory20)
                   .domain(['Biomass', 'Black coal', 'Liquid Fuel', 'Gas', 'Battery', 'Wind', 'Hydro', 'Brown coal'])
                   .range(["#99C24D", "#232020", "#553739", "#BB4430", "#E7BB41", "#8963BA", "#7EBDC2", "#DDA15E"]);
@@ -23,15 +19,12 @@ color_states = d3.scaleOrdinal()
                   .domain(states)
                   .range(["#ED8624", "#4C78A8", "#E45756", "#94C9A9", "#C6ECAE"]);
 
-// append the svg object to the body of the page
 var svg = d3.select("#fuel_sankey").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", 
-          "translate(" + margin.left + "," + margin.top + ")");
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-// Set the sankey diagram properties
 var sankey = d3.sankey()
     .nodeWidth(36)
     .nodePadding(40)
@@ -40,10 +33,8 @@ var sankey = d3.sankey()
 
 var path = sankey.link();
 
-// load the data
 d3.csv("./sankey/sankey_data.csv", function(error, data) {
  
-  //set up graph in same style as original example but empty
   graph = {"nodes" : [], "links" : []};
 
   data.forEach(function (d) {
@@ -54,19 +45,15 @@ d3.csv("./sankey/sankey_data.csv", function(error, data) {
                        "value": +d.value });
    });
 
-  // return only the distinct / unique nodes
   graph.nodes = d3.keys(d3.nest()
-    .key(function (d) { return d.name; })
-    .object(graph.nodes));
+      .key(function (d) { return d.name; })
+      .object(graph.nodes));
 
-  // loop through each link replacing the text with its index from node
   graph.links.forEach(function (d, i) {
     graph.links[i].source = graph.nodes.indexOf(graph.links[i].source);
     graph.links[i].target = graph.nodes.indexOf(graph.links[i].target);
   });
 
-  // now loop through each nodes to make nodes an array of objects
-  // rather than an array of strings
   graph.nodes.forEach(function (d, i) {
     graph.nodes[i] = { "name": d };
   });
@@ -76,7 +63,6 @@ d3.csv("./sankey/sankey_data.csv", function(error, data) {
       .links(graph.links)
       .layout(32);
 
-  // add in the links
   var link = svg.append("g").selectAll(".link")
       .data(graph.links)
       .enter().append("path")
@@ -86,19 +72,14 @@ d3.csv("./sankey/sankey_data.csv", function(error, data) {
       .style("stroke-width", function(d) { return Math.max(1, d.dy); })
       .sort(function(a, b) { return b.dy - a.dy; });
 
-  // add the link titles
   link.append("title")
-        .text(function(d) {
-    		return d.source.name + " → " + 
-                d.target.name + "\n" + format(d.value); });
+      .text(function(d) {return d.source.name + " → " + d.target.name + "\n" + format(d.value); });
 
-  // add in the nodes
   var node = svg.append("g").selectAll(".node")
       .data(graph.nodes)
       .enter().append("g")
       .attr("class", "node")
-      .attr("transform", function(d) { 
-		  return "translate(" + d.x + "," + d.y + ")"; })
+      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
       .call(d3.drag()
         .subject(function(d) {
           return d;
